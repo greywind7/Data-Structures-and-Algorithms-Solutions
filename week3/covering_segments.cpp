eb498af155c0f5016fb9f52e6986caab66007684
greywind7 <ritik.aggr@gmail.com>
2020-06-04 12:26:19 +0530

    Signatures problem done, car fuelling and largest number are still open

--- week3/covering_segments.cpp
+++ week3/covering_segments.cpp
@@ -1,20 +1,26 @@
-#include <algorithm>
-#include <iostream>
-#include <climits>
-#include <vector>
+#include<bits/stdc++.h>
 
-using std::vector;
+using namespace std;
 
-struct Segment {
-  int start, end;
-};
-
-vector<int> optimal_points(vector<Segment> &segments) {
+vector<int> optimal_points(map <int, pair<int, int> > &in_pts) {
   vector<int> points;
   //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+  for (size_t i = 0; i < in_pts.size(); ++i) {
+    
+    while(in_pts.size()!=0)
+    {
+      auto cur = in_pts.begin();
+      int cur_min = cur->first;
+      points.push_back(cur_min);
+      for(auto i = in_pts.begin(); i != in_pts.end();)
+      {
+        if(cur_min >= i->second.first && cur_min <= i->second.second)
+          in_pts.erase(i++->first);
+        else
+          ++i;
+      }
+    }
+    
   }
   return points;
 }
@@ -22,11 +28,16 @@ vector<int> optimal_points(vector<Segment> &segments) {
 int main() {
   int n;
   std::cin >> n;
-  vector<Segment> segments(n);
-  for (size_t i = 0; i < segments.size(); ++i) {
-    std::cin >> segments[i].start >> segments[i].end;
+  map <int, pair<int, int> > in_pts;
+  
+  for (int i = 0; i < n; i++)
+  {
+    int start, end;
+    cin >> start >> end;
+    in_pts[end] = make_pair(start,end);
   }
-  vector<int> points = optimal_points(segments);
+  
+  vector<int> points = optimal_points(in_pts);
   std::cout << points.size() << "\n";
   for (size_t i = 0; i < points.size(); ++i) {
     std::cout << points[i] << " ";

be3c603723b59e63089d851f3af2ffb08637cc68
greywind7 <ritik.aggr@gmail.com>
2020-06-08 12:33:00 +0530

    Majority element problem done

--- week4/majority_element.cpp
+++ week4/majority_element.cpp
@@ -1,14 +1,14 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
-using std::vector;
+using namespace std;
 
-int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
+int get_majority_element(vector<int> &a) {
   //write your code here
-  return -1;
+  unordered_map<int,int> freq;
+  for(int i:a) freq[i]++;
+  int mj = a.size()/2;
+  for(auto j:freq) if(j.second > mj) return 1;
+  return 0;
 }
 
 int main() {
@@ -18,5 +18,5 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  std::cout << get_majority_element(a) << '\n';
 }

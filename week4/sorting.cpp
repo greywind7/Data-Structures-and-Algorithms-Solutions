dd66c85a7154517c668c48645e399db8c3a56f73
greywind7 <ritik.aggr@gmail.com>
2020-06-09 08:14:31 +0530

    Sorting done and open updated

--- week4/sorting.cpp
+++ week4/sorting.cpp
@@ -1,9 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
+#include<bits/stdc++.h>
 
-using std::vector;
-using std::swap;
+using namespace std;
 
 int partition2(vector<int> &a, int l, int r) {
   int x = a[l];
@@ -18,6 +15,25 @@ int partition2(vector<int> &a, int l, int r) {
   return j;
 }
 
+ pair<int,int> partition3(vector<int> &a, int l, int r) {
+  int x = a[l];
+  int j = l;
+  for (int i = l + 1; i <= r; i++) {
+    if (a[i] <= x) {
+      j++;
+      swap(a[i], a[j]);
+    }
+  }
+  swap(a[l], a[j]);
+  int k = j;
+  while(1)
+  {
+    if(a[k-1] == a[j]) k--;
+    else break;
+  }
+  return {k,j};
+}
+
 void randomized_quick_sort(vector<int> &a, int l, int r) {
   if (l >= r) {
     return;
@@ -25,10 +41,11 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  int m1 = partition3(a, l, r).first;
+  int m2 = partition3(a, l, r).second;
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  randomized_quick_sort(a, l, m1 - 1);
+  randomized_quick_sort(a, m2 + 1, r);
 }
 
 int main() {

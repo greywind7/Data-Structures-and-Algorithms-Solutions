ed2b751f45302039267f10ae2440bb0328f3b424
greywind7 <ritik.aggr@gmail.com>
2020-06-10 10:21:41 +0530

    inversions intermediate

--- 1.Algorithmic_Toolbox/week4/inversions.cpp
+++ 1.Algorithmic_Toolbox/week4/inversions.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 
-using std::vector;
+using namespace std;
 
 long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
   long long number_of_inversions = 0;
@@ -13,6 +12,58 @@ long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, 
   return number_of_inversions;
 }
 
+int merge_inver(vector<int>&X, int l, int r)
+{
+    if(l == r) return 0;
+    // cout << "ONE" << endl;
+
+    int m = (l+r)/2;
+    long li = merge_inver(X,l,m), ri = merge_inver(X,m+1,r),inv = li + ri;
+    vector<int> temp;
+    int i = l, j = m + 1;
+
+    while(i < m+1 || j < r+1)
+    {
+        // cout << "TWO" << endl;
+        if(i <= m && j <= r)
+        {
+            if(X[i] > X[j])
+            {
+              // cout << "THREE" << endl;
+              inv += m - i + 1;
+              temp.push_back(X[j++]);
+            }
+            else 
+                temp.push_back(X[i++]);
+        }
+        else if (i <= m && j > r)
+        {
+            // cout << "FOUR" << endl;
+            while(i <= m)
+                temp.push_back(X[i++]);
+            i++;
+        }
+        else if(i > m && j <= r)
+        {
+            // cout << "FIVE" << endl;
+            while(j <= r)
+                temp.push_back(X[j++]);
+            j++;
+        }
+    }
+
+    auto ti = temp.begin();
+    while(l < r && ti != temp.end())
+    {
+      // cout << "SIX" << endl;
+      X[l] = *ti;
+      l++;
+      ti++;
+    }
+    temp.clear();
+    return inv;
+}
+
 int main() {
   int n;
   std::cin >> n;
@@ -21,5 +72,7 @@ int main() {
     std::cin >> a[i];
   }
   vector<int> b(a.size());
-  std::cout << get_number_of_inversions(a, b, 0, a.size()) << '\n';
+  std::cout << merge_inver(a,0,a.size()-1) << '\n';
+  // for(int i:a)
+  //   cout << i << ' ';
 }

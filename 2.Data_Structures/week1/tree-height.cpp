1dcb28b295912504ec09f9198ddd2d074f4e42c9
greywind7 <ritik.aggr@gmail.com>
2021-05-27 10:35:04 +0530

    Added recursive solution, no response on test case

--- 2.Data_Structures/week1/tree-height.cpp
+++ 2.Data_Structures/week1/tree-height.cpp
@@ -1,79 +1,37 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#if defined(__unix__) || defined(__APPLE__)
-#include <sys/resource.h>
-#endif
+#include<bits/stdc++.h>
 
-class Node;
+using namespace std;
 
-class Node {
-public:
-    int key;
-    Node *parent;
-    std::vector<Node *> children;
-
-    Node() {
-      this->parent = NULL;
+int getHeight(int node, vector< queue<int> >&tree)
+{
+  if(tree[node].empty())
+    return 1;
+  else{
+    // cout << "WEEEE\n";
+    int height = INT_MIN;
+    queue<int>temp = tree[node];
+    while(!temp.empty()){
+      height = max(height,getHeight(temp.front(),tree));
+      temp.pop();
     }
-
-    void setParent(Node *theParent) {
-      parent = theParent;
-      parent->children.push_back(this);
-    }
-};
-
-
-int main_with_large_stack_space() {
-  std::ios_base::sync_with_stdio(0);
-  int n;
-  std::cin >> n;
-
-  std::vector<Node> nodes;
-  nodes.resize(n);
-  for (int child_index = 0; child_index < n; child_index++) {
-    int parent_index;
-    std::cin >> parent_index;
-    if (parent_index >= 0)
-      nodes[child_index].setParent(&nodes[parent_index]);
-    nodes[child_index].key = child_index;
+    return height + 1;
   }
-
-  // Replace this code with a faster implementation
-  int maxHeight = 0;
-  for (int leaf_index = 0; leaf_index < n; leaf_index++) {
-    int height = 0;
-    for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-      height++;
-    maxHeight = std::max(maxHeight, height);
-  }
-    
-  std::cout << maxHeight << std::endl;
-  return 0;
+  
 }
 
-int main (int argc, char **argv)
+int main()
 {
-#if defined(__unix__) || defined(__APPLE__)
-  // Allow larger stack space
-  const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
-  struct rlimit rl;
-  int result;
-
-  result = getrlimit(RLIMIT_STACK, &rl);
-  if (result == 0)
+  int n, root;
+  cin >> n;
+  vector< queue<int> >tree(110000);
+  for(int i=0;i<n;i++)
   {
-      if (rl.rlim_cur < kStackSize)
-      {
-          rl.rlim_cur = kStackSize;
-          result = setrlimit(RLIMIT_STACK, &rl);
-          if (result != 0)
-          {
-              std::cerr << "setrlimit returned result = " << result << std::endl;
-          }
-      }
+    int x;
+    cin >> x;
+    if(x == -1)
+      root = i;
+    else
+      tree[x].push(i);
   }
-
-#endif
-  return main_with_large_stack_space();
+  cout << getHeight(root,tree) << endl;
 }
